795aa3286727f899a1b46e2810201f27e0f57fb4
eleder89 <eder.velazquez28@gmail.com>
2025-07-12 02:50:59 -0600

    primer commit

---
+++ bin/capitalize.js
@@ -0,0 +1,10 @@
+const capitalize = (text) => {
+  if (text.length === 0) {
+    return '';
+  }
+  const firstChar = text[0].toUpperCase();
+  const restSubstring = text.slice(1);
+  return `${firstChar}${restSubstring}`;
+};
+
+export default capitalize;
